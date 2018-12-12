a455cee3183ba75c6cbd14aa02aad39099b9f12f
vblanes <r4ven@hotmail.es>
2018-12-12 21:33:12 +0100

    Fast auto commit

--- alphabot.py
+++ alphabot.py
@@ -689,23 +689,23 @@ def main():
         # obten los mensajes no vistos
         updates = get_updates(last_update_id)
         # si hay algun mensaje do work
-        #try:
-        if 'result' in updates and len(updates['result']) > 0:  # REVIEW provisional patch for result error
-            last_update_id = get_last_update_id(updates) + 1
-            handle_updates(updates, debug)
-            # have to be gentle with the telegram server
-            time.sleep(0.5)
-        else:
-            # if no messages lets be *more* gentle with telegram servers
-            time.sleep(1)
-        '''
+        try:
+            if 'result' in updates and len(updates['result']) > 0:  # REVIEW provisional patch for result error
+                last_update_id = get_last_update_id(updates) + 1
+                handle_updates(updates, debug)
+                # have to be gentle with the telegram server
+                time.sleep(0.5)
+            else:
+                # if no messages lets be *more* gentle with telegram servers
+                time.sleep(1)
+
         except Exception as e:
             print('Error ocurred, watch log!')
             log_entry(str(e))
             print(e)
             # sleep 20 seconds so the problem may solve
             time.sleep(20)
-        '''
+        
 
 
 if __name__ == '__main__':
