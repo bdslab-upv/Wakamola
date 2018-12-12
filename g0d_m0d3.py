1b642f3809111b3de7fb1ea1e8650fc44073c513
vblanes <r4ven@hotmail.es>
2018-12-12 22:22:51 +0100

    Fast auto commit

--- g0d_m0d3.py
+++ g0d_m0d3.py
@@ -3,9 +3,9 @@ from dbhelper import DBHelper
 def h4ck(id_user):
     db = DBHelper()
     nq_category = db.n_questions()
-    for i, in list(nq_category.keys()):
-        for j in range(1, nq_category[categories]+1):
+    for i in list(nq_category.keys()):
+        for j in range(1, nq_category[i]+1):
             if i == 1 and j > 5:
-                add_answer(id_user, i, j, -1, 0)
+                db.add_answer(id_user, i, j, -1, 0)
             else:
-                add_answer(id_user, i, j, -1, 49)                
+                db.add_answer(id_user, i, j, -1, 49)
