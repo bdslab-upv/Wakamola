876ce1c1cede457c98ed289f8d2e51ed775f0cab
vicent <viblasel@inf.upv.es>
2019-12-03 17:33:55 +0100

    update

--- __developer_utils/run.sh
+++ __developer_utils/run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 rm -r __pycache__
-python wakamola.py -l es --godmode wakafill --statistics statistics --network create_graph --network_link "158.42.166.224/wakamolaupv/html/index.html" &
+python wakamola.py -l es --godmode wakafill --statistics statistics --network create_graph --network_link "158.42.166.224/wakamolaupv" &
 apache2ctl -D FOREGROUND
